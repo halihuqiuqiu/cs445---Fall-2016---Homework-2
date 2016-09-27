21833b7b0087896b39522995459c9f7d8f97a44b
Yong Yang <yongyang3800@gmail.com>
2016-09-27 14:06:11 -0500

    UML 3

--- src/main/java/edu/iit/cs445/hw2/uml/F.java
+++ src/main/java/edu/iit/cs445/hw2/uml/F.java
@@ -4,21 +4,13 @@ package edu.iit.cs445.hw2.uml;
  * Created by YongYang on 9/27/16.
  */
 public class F {
-    private D d1,d2;
+    private D[] dArray = new D[2];
 
-    public D getD1() {
-        return d1;
+    public D[] getdArray() {
+        return dArray;
     }
 
-    public void setD1(D d1) {
-        this.d1 = d1;
-    }
-
-    public D getD2() {
-        return d2;
-    }
-
-    public void setD2(D d2) {
-        this.d2 = d2;
+    public void setdArray(D[] dArray) {
+        this.dArray = dArray;
     }
 }
