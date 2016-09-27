3a6caf380cfd1f58245d4c745440c5114a119c9f
Yong Yang <yongyang3800@gmail.com>
2016-09-27 14:00:04 -0500

    UML change

--- src/main/java/edu/iit/cs445/hw2/uml/D.java
+++ src/main/java/edu/iit/cs445/hw2/uml/D.java
@@ -5,7 +5,7 @@ package edu.iit.cs445.hw2.uml;
  */
 public class D {
     private B b1;
-    private F f1,f2;
+    private F f1,f2,f3,f4,f5;
 
     public B getB1() {
         return b1;
@@ -29,5 +29,29 @@ public class D {
 
     public void setF2(F f2) {
         this.f2 = f2;
+    }
+
+    public F getF3() {
+        return f3;
+    }
+
+    public void setF3(F f3) {
+        this.f3 = f3;
+    }
+
+    public F getF4() {
+        return f4;
+    }
+
+    public void setF4(F f4) {
+        this.f4 = f4;
+    }
+
+    public F getF5() {
+        return f5;
+    }
+
+    public void setF5(F f5) {
+        this.f5 = f5;
     }
 }
--- src/main/java/edu/iit/cs445/hw2/uml/F.java
+++ src/main/java/edu/iit/cs445/hw2/uml/F.java
@@ -4,7 +4,7 @@ package edu.iit.cs445.hw2.uml;
  * Created by YongYang on 9/27/16.
  */
 public class F {
-    private D d1,d2,d3,d4,d5;
+    private D d1,d2;
 
     public D getD1() {
         return d1;
@@ -20,29 +20,5 @@ public class F {
 
     public void setD2(D d2) {
         this.d2 = d2;
-    }
-
-    public D getD3() {
-        return d3;
-    }
-
-    public void setD3(D d3) {
-        this.d3 = d3;
-    }
-
-    public D getD4() {
-        return d4;
-    }
-
-    public void setD4(D d4) {
-        this.d4 = d4;
-    }
-
-    public D getD5() {
-        return d5;
-    }
-
-    public void setD5(D d5) {
-        this.d5 = d5;
     }
 }
